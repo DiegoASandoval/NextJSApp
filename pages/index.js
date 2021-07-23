f67f8a290c902db3996cbac1229080f9b346a31a
Diego Sandoval <sc.diego96@gmail.com>
2021-07-23 18:10:13 -0500

    Testing

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,100 @@
-import Head from "next/head";
-import { MongoClient } from "mongodb";
+// import Head from "next/head";
+// import { MongoClient } from "mongodb";
 
-import MeetupList from "../components/meetups/MeetupList";
+// import MeetupList from "../components/meetups/MeetupList";
+
+// function HomePage(props) {
+//   console.log(props.meetups);
+//   return (
+//     <>
+//       <Head>
+//         <title>Meetups</title>
+//         <meta
+//           name="description"
+//           content="Browse meetups and find one near you"
+//         ></meta>
+//       </Head>
+//       <MeetupList meetups={props.meetups} />
+//     </>
+//   );
+// }
+
+// export async function getStaticProps() {
+//   const client = await MongoClient.connect(
+//     "mongodb+srv://diego:manejadatos123@cluster0.4njzt.mongodb.net/meetups?retryWrites=true&w=majority"
+//   );
+//   const db = client.db();
+
+//   const meetupsCollection = db.collection("meetups");
+
+//   const meetups = await meetupsCollection.find().toArray();
+
+//   client.close();
+
+//   fetch("");
+
+//   return {
+//     props: {
+//       meetups: meetups.map((meetup) => ({
+//         title: meetup.title,
+//         address: meetup.address,
+//         image: meetup.image,
+//         id: meetup._id.toString(),
+//       })),
+//     },
+//     revalidate: 1,
+//   };
+// }
+
+// export default HomePage;
+
+
+import { Fragment } from 'react';
+import Head from 'next/head';
+import { MongoClient } from 'mongodb';
+
+import MeetupList from '../components/meetups/MeetupList';
 
 function HomePage(props) {
-  console.log(props.meetups);
   return (
-    <>
+    <Fragment>
       <Head>
-        <title>Meetups</title>
+        <title>React Meetups</title>
         <meta
-          name="description"
-          content="Browse meetups and find one near you"
-        ></meta>
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
       </Head>
-      <MeetupList meetups={props.meetups} />
-    </>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
   );
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
 export async function getStaticProps() {
+  // fetch data from an API
   const client = await MongoClient.connect(
     "mongodb+srv://diego:manejadatos123@cluster0.4njzt.mongodb.net/meetups?retryWrites=true&w=majority"
   );
   const db = client.db();
 
-  const meetupsCollection = db.collection("meetups");
+  const meetupsCollection = db.collection('meetups');
 
   const meetups = await meetupsCollection.find().toArray();
 
   client.close();
-
-  fetch("");
 
   return {
     props: {
